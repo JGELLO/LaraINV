af08c9b56623036453d4611327be22286f09953f
Angelo Alcera Cabaluna <cabsgelo561561@gmail.com>
2023-02-15 14:55:30 +0800

    new file:   .editorconfig 	new file:   .env.example 	new file:   .gitattributes 	new file:   .gitignore 	modified:   README.md 	new file:   app/Console/Kernel.php 	new file:   app/Exceptions/Handler.php 	new file:   app/Http/Controllers/Controller.php 	new file:   app/Http/Kernel.php 	new file:   app/Http/Middleware/Authenticate.php 	new file:   app/Http/Middleware/EncryptCookies.php 	new file:   app/Http/Middleware/PreventRequestsDuringMaintenance.php 	new file:   app/Http/Middleware/RedirectIfAuthenticated.php 	new file:   app/Http/Middleware/TrimStrings.php 	new file:   app/Http/Middleware/TrustHosts.php 	new file:   app/Http/Middleware/TrustProxies.php 	new file:   app/Http/Middleware/ValidateSignature.php 	new file:   app/Http/Middleware/VerifyCsrfToken.php 	new file:   app/Models/Listing.php 	new file:   app/Models/User.php 	new file:   app/Providers/AppServiceProvider.php 	new file:   app/Providers/AuthServiceProvider.php 	new file:   app/Providers/BroadcastServiceProvider.php 	new file:   app/Providers/EventServiceProvider.php 	new file:   app/Providers/RouteServiceProvider.php 	new file:   artisan 	new file:   bootstrap/app.php 	new file:   bootstrap/cache/.gitignore 	new file:   composer.json 	new file:   composer.lock 	new file:   config/app.php 	new file:   config/auth.php 	new file:   config/broadcasting.php 	new file:   config/cache.php 	new file:   config/cors.php 	new file:   config/database.php 	new file:   config/filesystems.php 	new file:   config/hashing.php 	new file:   config/logging.php 	new file:   config/mail.php 	new file:   config/queue.php 	new file:   config/sanctum.php 	new file:   config/services.php 	new file:   config/session.php 	new file:   config/view.php 	new file:   database/.gitignore 	new file:   database/factories/ListingFactory.php 	new file:   database/factories/UserFactory.php 	new file:   database/migrations/2014_10_12_000000_create_users_table.php 	new file:   database/migrations/2014_10_12_100000_create_password_resets_table.php 	new file:   database/migrations/2019_08_19_000000_create_failed_jobs_table.php 	new file:   database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php 	new file:   database/migrations/2023_02_15_054936_create_listings_table.php 	new file:   database/seeders/DatabaseSeeder.php 	new file:   lang/en/auth.php 	new file:   lang/en/pagination.php 	new file:   lang/en/passwords.php 	new file:   lang/en/validation.php 	new file:   package.json 	new file:   phpunit.xml 	new file:   public/.htaccess 	new file:   public/favicon.ico 	new file:   public/index.php 	new file:   public/robots.txt 	new file:   resources/css/app.css 	new file:   resources/js/app.js 	new file:   resources/js/bootstrap.js 	new file:   resources/views/listing.blade.php 	new file:   resources/views/listings.blade.php 	new file:   routes/api.php 	new file:   routes/channels.php 	new file:   routes/console.php 	new file:   routes/web.php 	new file:   storage/app/.gitignore 	new file:   storage/app/public/.gitignore 	new file:   storage/framework/.gitignore 	new file:   storage/framework/cache/.gitignore 	new file:   storage/framework/cache/data/.gitignore 	new file:   storage/framework/sessions/.gitignore 	new file:   storage/framework/testing/.gitignore 	new file:   storage/framework/views/.gitignore 	new file:   storage/logs/.gitignore 	new file:   tests/CreatesApplication.php 	new file:   tests/Feature/ExampleTest.php 	new file:   tests/TestCase.php 	new file:   tests/Unit/ExampleTest.php 	new file:   vite.config.js

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      //  \App\Models\User::factory()->create([
+      //       'name' => 'Test User',
+      //     'email' => 'test@example.com',
+     //    ]);
+
+     \App\Models\User::factory(5)->create();
+
+    Listing::factory(10)->create();
+
+   /*  Listing::create([
+        'title' => 'Senior Developer',
+        'tags' => 'Java',
+        'company' => 'Hipop',
+        'location' => 'Makati ako',
+        'email' => 'gadg@email.com',
+        'website' => 'ssss',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ornare elementum nunc. Cras id vulputate purus, ut feugiat nisi. Sed eu accumsan ex, congue semper mi. Nam enim tortor, commodo eget facilisis sed, semper nec nisl. Pellentesque semper tellus nec quam tempus, eget tincidunt ante dignissim. Aenean sed vehicula massa. Mauris aliquam orci vitae hendrerit tincidunt. Etiam nec leo ac orci elementum fringilla.',
+    ]);
+
+    Listing::create([
+        'title' => 'Junior Developer',
+        'tags' => 'HTML',
+        'company' => 'piperps',
+        'location' => 'cdoc',
+        'email' => 'hffg@email.com',
+        'website' => 'ffff',
+        'description' => 'Integer quam ligula, tincidunt eu orci sed, ullamcorper tempus erat. Sed sit amet augue pellentesque purus auctor efficitur. Fusce auctor ligula nec metus placerat, nec ultricies justo tincidunt. Phasellus mattis ipsum nec diam volutpat pretium. Quisque volutpat vehicula facilisis. Aenean in neque interdum massa mollis facilisis vitae eu sapien. Duis lacinia tincidunt venenatis. Proin ultrices purus eu malesuada accumsan. Ut maximus tristique urna, ut ultrices elit cursus et. Ut ante turpis, placerat ut sodales pulvinar, hendrerit id lectus.',
+    ]); */
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// All Listings
+Route::get('/', function () {
+    return view('listings',[
+        'heading' => 'Latest yes',
+        'listings' => Listing::all()
+    ]);
+}); 
+
+// Single Listing
+Route::get('/listings/{id}', function($id){
+    return view('listing', [
+        'listing' => Listing::find($id)
+    ]);
+});
